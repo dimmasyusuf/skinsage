7ed3e765bca8b2ab491932fb484b08d04d3a38e7
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-10-20 13:15:31 +0700

    fix: change padding at blockquote

--- src/components/sections/Blockquote.tsx
+++ src/components/sections/Blockquote.tsx
@@ -2,17 +2,17 @@ import {Quote} from 'lucide-react';
 
 export default function Blockquote() {
   return (
-    <section className="bg-secondary w-full">
-      <div className="mx-auto flex max-w-screen-lg flex-col items-center justify-center gap-4 px-4 py-8 md:px-6">
-        <Quote className="text-primary fill-primary" />
+    <section className="w-full bg-secondary px-4 md:px-6">
+      <div className="mx-auto flex max-w-screen-lg flex-col items-center justify-center gap-4 px-4 py-12 md:px-6">
+        <Quote className="fill-primary text-primary" />
 
-        <blockquote className="prose prose-sm md:prose-base lg:prose-lg text-center font-medium">
+        <blockquote className="prose prose-sm text-center font-medium md:prose-base lg:prose-lg">
           ”I created SkinSage because everyone deserves clear and healthy skin, without worrying
           about money. Growing up in a small town, I saw how hard it was for my family to get good
           skincare advice. That&apos;s why I wanted to make it easier for everyone, no matter where
           they live”
         </blockquote>
-        <cite className="prose prose-sm md:prose-base lg:prose-lg text-center font-medium">
+        <cite className="prose prose-sm text-center font-medium md:prose-base lg:prose-lg">
           - Vivekananda
         </cite>
       </div>
